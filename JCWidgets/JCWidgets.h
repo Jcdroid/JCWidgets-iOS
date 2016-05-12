4ec79523fc5132877e8c21fb1cf8b5e9f8888ca5
caojing <438339197@qq.com>
2016-05-12 11:39:19 +0800

    Initial Commit

---
+++ JCWidgets/JCWidgets.h
@@ -0,0 +1,19 @@
+//
+//  JCWidgets.h
+//  JCWidgets
+//
+//  Created by mzy on 16/5/12.
+//  Copyright © 2016年 jcdroid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JCWidgets.
+FOUNDATION_EXPORT double JCWidgetsVersionNumber;
+
+//! Project version string for JCWidgets.
+FOUNDATION_EXPORT const unsigned char JCWidgetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JCWidgets/PublicHeader.h>
+
+
